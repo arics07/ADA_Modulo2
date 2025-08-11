8494cfc7dfef3165a144c2fbb334e5062c44ab4e
Ariadna Salomone <aricsalomone@gmail.com>
2025-08-11 20:26:58 -0300

    Subo ejercicios

---
+++ Clase 3/Ejercicio3/index.js
@@ -0,0 +1,16 @@
+/*
+Actividad 3: Registro de tareas pendientes
+Consigna: Crea un programa para gestionar tus tareas pendientes. El programa debe permitirte:
+1. Agregar una tarea: Registrar una tarea pendiente con su descripción.
+2. Listar las tareas: Mostrar todas las tareas registradas.
+3. Eliminar una tarea: Eliminar una tarea de la lista.
+Pistas:
+• Usa un archivo JSON para guardar las tareas.
+• Si el archivo no existe, comienza con un arreglo vacío.
+• Eliminar una tarea se puede hacer por su ID.
+*/
+
+const fs = require('fs');
+
+const filePath = './tareas.json';
+
---
+++ Clase 3/Ejercicio4/index.js
@@ -0,0 +1,15 @@
+/*
+Actividad 4: Control de inventario
+Consigna: Crea un programa para llevar el control de un inventario. El
+programa debe permitirte:
+1. Agregar un producto: Registrar un producto con su nombre y
+cantidad disponible.
+2. Listar los productos: Mostrar todos los productos registrados.
+3. Actualizar la cantidad de un producto: Modificar la cantidad de
+un producto en el inventario.
+Pistas:
+• Usa un archivo JSON para guardar los productos.
+• Si el archivo no existe, comienza con un arreglo vacío.
+• Cada producto debe tener un ID único que se pueda utilizar para
+actualizar su cantidad.
+*/
