a1489d0310ad8c4b8fb785570571d5895a665437
Ariadna Salomone <aricsalomone@gmail.com>
2025-09-20 16:46:48 -0300

    Subo ejercicios

---
+++ Clase 12/Ejercicios/Ejercicio 1/client.js
@@ -0,0 +1,54 @@
+const net = require('net');
+//np, install readline-sync
+const readlineSync = require('readline-sync');
+
+const PORT = 8080;
+const HOST = 'localhost';
+
+const client = net.createConnection({port: PORT, host: HOST});
+
+//Evento 'connect':para manejar la conexión con el servidor
+client.on('connect', () => {
+    //Mensaje de conexión exitosa
+    console.log("Conectado al servidor.");
+
+    //Envía un mensaje al servidor al conectarse
+    client.write("C:\\Users\\Usuario\\Pictures\\escuela.png");
+});
+
+//Evento 'data': manejo de datos recibidos desde el servidor
+client.on('data', (data) => {
+    console.log(`Mensaje recibido del servidor: ${data.toString()}`);
+    sendLine();
+});
+
+//Evento 'end': se ejecuta cuando el servidor cierra la conexión
+client.on('end', () => {
+    console.log("El servidor ha cerrado la conexión.");
+});
+
+//Evento 'close': se ejecuta cuando la conexión se cierra completamente, tanto del lado del servidor como del cliente, ordenadamente
+client.on('close', () => {
+    console.log("Conexión cerrada con éxito.");
+});
+
+//Evento 'error': maneja errores en la conexión
+client.on('error', (err) => {
+    console.error(`Error en la conexión: ${err.message}`);
+});
+
+//Cerramos la conexión si no hay respuesta en 20 segundos
+client.setTimeout(20000, () => {
+    console.log("Tiempo de espera agotado. Cerrando la conexión...");
+    client.end(); //Finaliza la conexión de forma ordenada
+});
+
+//Esta función permite ingresasr un mensaje por la terminal
+function sendLine() {
+    var line = readlineSync.question("Ingrese una ruta: ");
+    if (line === '0') {
+        client.end(); //si el usuario ingresa 0, se cierra la conexión 
+    } else {
+        client.write(line); //envía la información al servidor
+    };
+};
---
+++ Clase 12/Ejercicios/Ejercicio 1/server.js
@@ -0,0 +1,43 @@
+const net = require('net'); //se usa para crear servidores y clientes
+const path = require('path');
+
+const server = net.createServer();
+
+//Manejo del evento 'connection' que se emite cuando el cliente le manda un mensaje al servidor
+server.on('connection', (socket) => {
+    
+    console.log(`El ciente ${socket.remoteAddress}:${socket.remotePort} se ha conectado`);
+
+    socket.on('data', (data) => {
+        console.log(`El ciente ${socket.remoteAddress}:${socket.remotePort} dice: ${data.toString()}`);
+        const formattedData = data.toString().trim();
+        if (formattedData === "0") {
+            socket.end();
+            return
+        };
+        socket.write("Mensaje recibido.");
+        const esRutaAbsoluta = path.isAbsolute(formattedData);
+        (esRutaAbsoluta) ? socket.write("La ruta recibida es una ruta absoluta") : socket.write("La ruta recibida NO es una ruta absoluta");
+    });
+
+    socket.on('close', () => {
+        console.log("El cliente se ha desconectado. Comunicación finalizada.");
+    });
+
+    socket.on('error', (err) => {
+        console.error(`Ocurrió un error: ${err.message}`);
+    });
+
+    socket.on('end', () => {
+        console.log("Conexión finalizada.");
+    })
+
+});
+
+
+//Iniciar el servidor
+const PORT = 8080;
+
+server.listen(PORT, () => {
+    console.log(`Servidor escuchado en el puerto ${server.address().port}`);
+});
