dabd0a8e3a92f12e4ac304c220b0bbaa8640cf14
Ariadna Salomone <aricsalomone@gmail.com>
2025-10-02 16:25:54 -0300

    Subo ejercicios

---
+++ Clase 14/Ejemplos/1.CreateHash.js
@@ -0,0 +1,17 @@
+// Importamos el módulo 'crypto' en Node
+const crypto = require('crypto');
+
+// Datos que queremos hashear
+const data = 'Este es un mensaje secreto, aunque todos saben que mi micho es el más hermosooo!';
+
+// Creamos el objeto hash usando el algoritmo 'sha256'
+const hash = crypto.createHash('sha256');
+
+// Actualizar el hash con los datos
+hash.update(data);
+
+// Obtener el hash en formato hexadecimal
+const hashOutput = hash.digest('hex');
+
+// Mostrar el hash generado
+console.log('Hash de sha256: ', hashOutput);
